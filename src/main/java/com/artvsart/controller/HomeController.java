1691deebc65138ac1e18c47b472c0433fa7517db
Blake <bwilfong@depaul.edu>
2026-09-01 10:01:06 -0500

    update HomeController. model is a container for data sent to ThymeLeaf

--- src/main/java/com/artvsart/controller/HomeController.java
+++ src/main/java/com/artvsart/controller/HomeController.java
@@ -1,14 +1,22 @@
 package com.artvsart.controller;
 
+import com.artvsart.service.ArtworkService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
+    private final ArtworkService artworkService;
+
+    public HomeController(ArtworkService artworkService) {
+        this.artworkService = artworkService;
     }
 
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("artworks", artworkService.getAllArtworks());
+        return "home";
+    }
 }
