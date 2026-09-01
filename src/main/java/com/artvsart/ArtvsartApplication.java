9935f4ee6dd74a521b27632b4180b37c4ad83e78
Blake <bwilfong@depaul.edu>
2026-09-01 09:30:41 -0500

    Initialize Spring Boot project

---
+++ src/main/java/com/artvsart/ArtvsartApplication.java
@@ -0,0 +1,13 @@
+package com.artvsart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtvsartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtvsartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artvsart/ArtvsartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artvsart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtvsartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
