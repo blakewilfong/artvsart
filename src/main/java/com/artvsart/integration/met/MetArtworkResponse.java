c8e8b39f75f1d380a88a350e6790e8b02b3743f1
Blake <bwilfong@depaul.edu>
2026-09-02 15:01:39 -0500

    expand Met artwork metadata response

--- src/main/java/com/artvsart/integration/met/MetArtworkResponse.java
+++ src/main/java/com/artvsart/integration/met/MetArtworkResponse.java
@@ -9,24 +9,64 @@ public record MetArtworkResponse(
         @JsonProperty("objectID")
         Long objectId,
 
+        @JsonProperty("isHighlight")
+        Boolean highlight,
+
         @JsonProperty("isPublicDomain")
-        boolean publicDomain,
+        Boolean publicDomain,
+
+        String accessionYear,
+
+        String primaryImage,
 
         String primaryImageSmall,
 
+        String department,
+
+        String objectName,
+
         String title,
+
+        String culture,
+
+        String period,
 
         String artistDisplayName,
 
+        String artistDisplayBio,
+
+        String artistNationality,
+
+        String artistBeginDate,
+
+        String artistEndDate,
+
         String objectDate,
 
+        Integer objectBeginDate,
+
+        Integer objectEndDate,
+
+        String medium,
+
+        String dimensions,
+
+        String country,
+
+        String classification,
+
+        String creditLine,
+
         @JsonProperty("objectURL")
-        String objectUrl
+        String objectUrl,
+
+        @JsonProperty("objectWikidata_URL")
+        String objectWikidataUrl
 ) {
 
     public boolean isUsable() {
         return objectId != null
-                && publicDomain
+                && Boolean.TRUE.equals(publicDomain)
                 && hasText(title)
                 && hasText(artistDisplayName)
                 && hasText(objectDate)
